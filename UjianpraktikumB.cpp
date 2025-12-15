bae204b01d2b549452a560d8e8e34f844285ae3b
naufalnoah8 <naufalnoah8@gmail.com>
2025-12-14 20:52:54 -0800

    Add files via upload

---
+++ UjianpraktikumB.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Masukkan tinggi segitiga: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        
+        for (int j = i; j < n; j++) {
+            cout << " ";
+        }
+
+
+        for (int  k = 1;  k <= (2 * i - 1); k++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
